6bae1c91b602b6a5d31f89131f29e55a49905889
descawed <github@descawed.com>
2024-10-03 04:26:17 -0400

    - Patch weapon selection - Run rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,28 @@
 use std::ffi::c_void;
 use std::fs::File;
 use std::panic;
+
 use anyhow::{anyhow, Result};
 use simplelog::{Config, LevelFilter, WriteLogger};
 use windows::Win32::Foundation::{BOOL, HMODULE};
-use windows::Win32::System::Memory::{PAGE_READONLY, PAGE_READWRITE, PAGE_EXECUTE_READ, PAGE_WRITECOPY, PAGE_EXECUTE_READWRITE};
+use windows::Win32::System::Memory::{
+    PAGE_EXECUTE_READ, PAGE_EXECUTE_READWRITE, PAGE_READONLY, PAGE_READWRITE, PAGE_WRITECOPY,
+};
 use windows::Win32::System::SystemServices::DLL_PROCESS_ATTACH;
 
-mod patch;
 mod game;
+mod patch;
 
-const ICON_TEX_NAME: &'static [u8] = b"data/pic/etc/itemmenu2.tex";
+const ICON_TEX_NAME: &[u8] = b"data/pic/etc/itemmenu2.tex";
 const MARIA_ICON_DRAW_CALL: [u8; 7] = [0x50, 0x0F, 0xB6, 0x46, 0xF0, 0x50, 0xE8];
-const ICON_DRAW_LOOP: [u8; 16] = [0x66, 0x8B, 0x50, 0x04, 0x66, 0x2B, 0x10, 0x83, 0xC0, 0x3C, 0x66, 0x89, 0x51, 0xFE, 0x66, 0x8B];
+const ICON_DRAW_LOOP: [u8; 16] = [
+    0x66, 0x8B, 0x50, 0x04, 0x66, 0x2B, 0x10, 0x83, 0xC0, 0x3C, 0x66, 0x89, 0x51, 0xFE, 0x66, 0x8B,
+];
+const MARIA_WEAPON_ASSERT: [u8; 8] = [0xFF, 0x75, 0x1B, 0x68, 0x17, 0x03, 0x00, 0x00];
+const CHECK_JAMES_WEAPON_LIST: [u8; 7] = [
+    0x31, 0xD2, // xor edx, edx
+    0xE9, 0x9F, 0x00, 0x00, 0x00, // jmp +159 bytes
+];
 
 fn open_log() -> Result<()> {
     let log_file = File::create("sh2hvnknf.log")?;
@@ -47,29 +57,58 @@ fn main(reason: u32) -> Result<()> {
         [Some(tex_address)] => tex_address,
         _ => return Err(anyhow!("Failed to find item icon texture name")),
     };
-    log::debug!("Found item menu texture path at {:#08X}", tex_address as usize);
+    log::debug!(
+        "Found item menu texture path at {:#08X}",
+        tex_address as usize,
+    );
 
     let mut menu_data: [u8; 16] = [0, 0, 0, 0, 0xff, 0xff, 0xff, 0xff, 0, 0, 0, 0, 1, 0, 0, 0];
     menu_data[..4].copy_from_slice(&(tex_address as usize).to_le_bytes());
     // we already rely on our icon coords game constant being a binary match for the exe's icon
     // coords, so let's just directly use it as our search data as well
-    let icon_coords_ptr = (&game::ICON_COORDS).as_ptr() as *const u8;
+    let icon_coords_ptr = game::ICON_COORDS.as_ptr() as *const u8;
     let icon_coords_buf = unsafe { std::slice::from_raw_parts(icon_coords_ptr, 12) };
 
-    let (menu_address, icon_coord_address) = match searcher.find_bytes(&[&menu_data, icon_coords_buf], Some(PAGE_READWRITE | PAGE_WRITECOPY), sh2pc)? {
+    let (menu_address, icon_coord_address) = match searcher.find_bytes(
+        &[&menu_data, icon_coords_buf],
+        Some(PAGE_READWRITE | PAGE_WRITECOPY),
+        sh2pc,
+    )? {
         [Some(menu_address), Some(icon_coord_address)] => (menu_address, icon_coord_address),
         _ => return Err(anyhow!("Failed to find .data values")),
     };
-    log::debug!("Found menu data at {:#08X}, icon coords at {:#08X}", menu_address as usize, icon_coord_address as usize);
+    log::debug!(
+        "Found menu data at {:#08X}, icon coords at {:#08X}",
+        menu_address as usize,
+        icon_coord_address as usize,
+    );
 
     // Maria vs James texture check
     let mut tex_ref_data: [u8; 7] = [0x50, 0x68, 0, 0, 0, 0, 0xE8];
     tex_ref_data[2..6].copy_from_slice(&(menu_address as usize).to_le_bytes());
-    let (tex_ref_call_address, icon_draw_call_address, icon_draw_loop_address) = match searcher.find_bytes(&[&tex_ref_data, &MARIA_ICON_DRAW_CALL, &ICON_DRAW_LOOP], Some(PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE), sh2pc)? {
-        [Some(tex_ref_call_address), Some(icon_draw_call_address), Some(icon_draw_loop_address)] => (tex_ref_call_address, icon_draw_call_address, icon_draw_loop_address),
+    let (tex_ref_call_address, icon_draw_call_address, icon_draw_loop_address, weapon_assert_address) = match searcher
+        .find_bytes(
+            &[&tex_ref_data, &MARIA_ICON_DRAW_CALL, &ICON_DRAW_LOOP, &MARIA_WEAPON_ASSERT],
+            Some(PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE),
+            sh2pc,
+        )? {
+        [Some(tex_ref_call_address), Some(icon_draw_call_address), Some(icon_draw_loop_address), Some(weapon_assert_address)] => {
+            (
+                tex_ref_call_address,
+                icon_draw_call_address,
+                icon_draw_loop_address,
+                weapon_assert_address,
+            )
+        }
         _ => return Err(anyhow!("Failed to find code addresses")),
     };
-    log::debug!("Found tex ref data at {:#08X}, icon draw call at {:#08X}, icon draw loop at {:#08X}", tex_ref_call_address as usize, icon_draw_call_address as usize, icon_draw_loop_address as usize);
+    log::debug!(
+        "Found tex ref data at {:#08X}, icon draw call at {:#08X}, icon draw loop at {:#08X}, weapon assert at {:#08X}",
+        tex_ref_call_address as usize,
+        icon_draw_call_address as usize,
+        icon_draw_loop_address as usize,
+        weapon_assert_address as usize,
+    );
 
     unsafe {
         // sanity checks
@@ -103,43 +142,122 @@ fn main(reason: u32) -> Result<()> {
         let coords_index_address2 = icon_draw_loop_address.offset(299);
         patch::assert_byte(coords_index_address2, 0x0F)?; // movsx
 
+        // we don't patch this check so we can handle Maria's no-weapon animation
+        /*let weapon_player_check_address1 = weapon_assert_address.offset(-63);
+        patch::assert_byte(weapon_player_check_address1, 0x0F)?; // jnz*/
+
+        let weapon_player_check_address2 = weapon_assert_address.offset(357);
+        patch::assert_byte(weapon_player_check_address2, 0x75)?; // jnz
+
+        let maria_weapon_assert_address = weapon_assert_address.offset(3);
+        // no point asserting since this is still within our search string
+
+        // prepare to rearrange weapon data entries
+        let weapon_data_ptr_address = weapon_assert_address.offset(169);
+        patch::assert_byte(weapon_data_ptr_address, 0xA1)?;
+
+        let weapon_data_address = usize::from_le_bytes(std::slice::from_raw_parts(weapon_data_ptr_address.offset(1) as *const u8, size_of::<usize>()).try_into()?) as *mut u8;
+        patch::assert_byte(weapon_data_address, 0)?;
+
+        let james_weapon_end_address = weapon_data_address.offset(180);
+        patch::assert_byte(james_weapon_end_address, 0xFF)?;
+
+        let maria_weapon_cleaver_address = weapon_data_address.offset(240);
+        patch::assert_byte(maria_weapon_cleaver_address, 17)?;
+
+        let maria_weapon_end_address = weapon_data_address.offset(260);
+        patch::assert_byte(maria_weapon_end_address, 0xFF)?;
+
         let icon_coords_addr_bytes = (icon_coords_ptr as usize).to_le_bytes();
         let icon_coords_field2_addr_bytes = (icon_coords_ptr.offset(2) as usize).to_le_bytes();
-        let icon_coords_end_addr_bytes = (icon_coords_ptr.add(size_of_val(&game::ICON_COORDS)) as usize).to_le_bytes();
-        let item_ids_addr_bytes = ((&game::ICON_ITEM_IDS).as_ptr() as usize).to_le_bytes();
-        let icon_floats_addr_bytes = ((&game::ICON_FLOATS).as_ptr() as usize).to_le_bytes();
+        let icon_coords_end_addr_bytes =
+            (icon_coords_ptr.add(size_of_val(&game::ICON_COORDS)) as usize).to_le_bytes();
+        let item_ids_addr_bytes = (game::ICON_ITEM_IDS.as_ptr() as usize).to_le_bytes();
+        let icon_floats_addr_bytes = (game::ICON_FLOATS.as_ptr() as usize).to_le_bytes();
 
         // use new icon texture which includes DLC icons
-        log::info!("Applying texture name patch at address {:#08X}", tex_address as usize);
+        log::info!(
+            "Applying texture name patch at address {:#08X}",
+            tex_address as usize
+        );
         patch::patch(tex_address.offset(21), b"3")?;
 
         // always use main scenario icon texture
-        log::info!("Applying menu texture patch at address {:#08X}", tex_ref_check_address as usize);
+        log::info!(
+            "Applying menu texture patch at address {:#08X}",
+            tex_ref_check_address as usize
+        );
         patch::patch(tex_ref_check_address, &[0xEB])?;
 
         // always use main scenario icon dimensions
-        log::info!("Applying icon draw patch at address {:#08X}", icon_check_address as usize);
+        log::info!(
+            "Applying icon draw patch at address {:#08X}",
+            icon_check_address as usize
+        );
         patch::patch(icon_check_address, &[0xEB])?;
 
         // use our expanded icon coordinate array
-        log::info!("Applying icon coordinate patches at addresses {:#08X}, {:#08X}, {:#08X}, {:#08X}", coords_load_address as usize, coords_end_load_address as usize, coords_index_address1 as usize, coords_index_address2 as usize);
+        log::info!(
+            "Applying icon coordinate patches at addresses {:#08X}, {:#08X}, {:#08X}, {:#08X}",
+            coords_load_address as usize,
+            coords_end_load_address as usize,
+            coords_index_address1 as usize,
+            coords_index_address2 as usize
+        );
         patch::patch(coords_load_address.offset(1), &icon_coords_addr_bytes)?;
-        patch::patch(coords_end_load_address.offset(1), &icon_coords_end_addr_bytes)?;
+        patch::patch(
+            coords_end_load_address.offset(1),
+            &icon_coords_end_addr_bytes,
+        )?;
         patch::patch(coords_index_address1.offset(3), &icon_coords_addr_bytes)?;
-        patch::patch(coords_index_address2.offset(3), &icon_coords_field2_addr_bytes)?;
+        patch::patch(
+            coords_index_address2.offset(3),
+            &icon_coords_field2_addr_bytes,
+        )?;
 
         // use our expanded item ID array
-        log::info!("Applying icon item ID patch at address {:#08X}", id_check_address as usize);
+        log::info!(
+            "Applying icon item ID patch at address {:#08X}",
+            id_check_address as usize
+        );
         patch::patch(id_check_address.offset(3), &item_ids_addr_bytes)?;
 
         // use our expanded icon float array
-        log::info!("Applying icon float patches at addresses {:#08X}, {:#08X}", float_address1 as usize, float_address2 as usize);
+        log::info!(
+            "Applying icon float patches at addresses {:#08X}, {:#08X}",
+            float_address1 as usize,
+            float_address2 as usize
+        );
         patch::patch(float_address1.offset(3), &icon_floats_addr_bytes)?;
         patch::patch(float_address2.offset(3), &icon_floats_addr_bytes)?;
 
         // increase number of items
-        log::info!("Applying item count patch at address {:#08X}", item_num_address as usize);
-        patch::patch(item_num_address.offset(2), &(game::NUM_ITEMS as u8).to_le_bytes())?;
+        log::info!(
+            "Applying item count patch at address {:#08X}",
+            item_num_address as usize
+        );
+        patch::patch(
+            item_num_address.offset(2),
+            &(game::NUM_ITEMS as u8).to_le_bytes(),
+        )?;
+
+        // merge James and Maria's weapon lists into a single contiguous list
+        log::info!("Merging weapon lists");
+
+        let james_weapon_end = std::slice::from_raw_parts_mut(james_weapon_end_address, game::WEAPON_INFO_SIZE);
+        let maria_weapon_end = std::slice::from_raw_parts(maria_weapon_end_address, game::WEAPON_INFO_SIZE);
+        let maria_weapon_cleaver = std::slice::from_raw_parts_mut(maria_weapon_cleaver_address, game::WEAPON_INFO_SIZE);
+
+        james_weapon_end.copy_from_slice(maria_weapon_cleaver); // replace James' end marker with the cleaver
+        maria_weapon_cleaver.copy_from_slice(maria_weapon_end); // replace the cleaver with the end marker
+
+        // we now have every weapon in one big list, but we'll still start Maria at the old start of her list
+        // so she gets the proper animation for no weapon
+
+        // now we patch the logic
+        log::info!("Patching weapon selection logic at addresses {:#08X}, {:#08X}", maria_weapon_assert_address as usize, weapon_player_check_address2 as usize);
+        patch::patch(maria_weapon_assert_address, &CHECK_JAMES_WEAPON_LIST)?;
+        patch::patch(weapon_player_check_address2, &[0x90, 0x90])?; // nop out jump to always use James path
     }
 
     log::info!("All patches applied successfully");
@@ -156,5 +274,6 @@ extern "system" fn DllMain(_dll_module: HMODULE, reason: u32, _reserved: *const 
             log::error!("Fatal error: {e}");
             false
         }
-    }.into()
+    }
+    .into()
 }
--- src/patch.rs
+++ src/patch.rs
@@ -6,7 +6,8 @@ use memchr::memmem;
 use windows::core::PWSTR;
 use windows::Win32::Foundation::{HMODULE, MAX_PATH};
 use windows::Win32::System::Memory::{
-    VirtualProtect, VirtualQuery, MEMORY_BASIC_INFORMATION, PAGE_PROTECTION_FLAGS, PAGE_NOACCESS, PAGE_EXECUTE_READWRITE, MEM_COMMIT,
+    VirtualProtect, VirtualQuery, MEMORY_BASIC_INFORMATION, MEM_COMMIT, PAGE_EXECUTE_READWRITE,
+    PAGE_NOACCESS, PAGE_PROTECTION_FLAGS,
 };
 use windows::Win32::System::ProcessStatus::{
     EnumProcessModules, GetModuleBaseNameW, GetModuleInformation, MODULEINFO,
@@ -37,7 +38,9 @@ pub unsafe fn assert_byte<T>(addr: *const T, expected: u8) -> Result<()> {
     let actual = *(addr as *const u8);
     if actual != expected {
         let uaddr = addr as usize;
-        return Err(anyhow!("Expected {expected:#02X} at {uaddr:#08X} but found {actual:#02X}"));
+        return Err(anyhow!(
+            "Expected {expected:#02X} at {uaddr:#08X} but found {actual:#02X}"
+        ));
     }
 
     Ok(())
@@ -55,7 +58,11 @@ impl ByteSearcher {
         }
     }
 
-    pub fn find_bytes_in_ranges<'a, const N: usize>(patterns: &[&[u8]; N], protection: Option<PAGE_PROTECTION_FLAGS>, ranges: impl Iterator<Item = &'a (*const c_void, *const c_void)>) -> Result<[Option<*const c_void>; N]> {
+    pub fn find_bytes_in_ranges<'a, const N: usize>(
+        patterns: &[&[u8]; N],
+        protection: Option<PAGE_PROTECTION_FLAGS>,
+        ranges: impl Iterator<Item = &'a (*const c_void, *const c_void)>,
+    ) -> Result<[Option<*const c_void>; N]> {
         let mut addresses = [None; N];
         for &(start, end) in ranges {
             let mut addr = start;
@@ -66,7 +73,9 @@ impl ByteSearcher {
 
                 let mut memory_info = MEMORY_BASIC_INFORMATION::default();
                 log::debug!("Querying address {:#08X}", addr as usize);
-                let result = unsafe { VirtualQuery(Some(addr), &mut memory_info, size_of_val(&memory_info)) };
+                let result = unsafe {
+                    VirtualQuery(Some(addr), &mut memory_info, size_of_val(&memory_info))
+                };
                 if result == 0 {
                     break;
                 }
@@ -74,14 +83,21 @@ impl ByteSearcher {
                 let search_base = addr as *const u8;
                 addr = unsafe { memory_info.BaseAddress.add(memory_info.RegionSize) };
 
-                if memory_info.State != MEM_COMMIT || protection.is_some_and(|p| !p.contains(memory_info.Protect)) {
+                if memory_info.State != MEM_COMMIT
+                    || protection.is_some_and(|p| !p.contains(memory_info.Protect))
+                {
                     log::trace!("Skipping address {:#08X} due to state {:#08X} or protection (actual {:#08X}, expected {:#08X})", search_base as usize, memory_info.State.0, memory_info.Protect.0, protection.unwrap_or(PAGE_NOACCESS).0);
                     continue;
                 }
 
                 log::debug!("Searching address {:#08X}", search_base as usize);
-                let search_region = unsafe { std::slice::from_raw_parts(search_base, memory_info.RegionSize) };
-                for (&pattern, address) in patterns.iter().zip(addresses.iter_mut()).filter(|(_, a)| a.is_none()) {
+                let search_region =
+                    unsafe { std::slice::from_raw_parts(search_base, memory_info.RegionSize) };
+                for (&pattern, address) in patterns
+                    .iter()
+                    .zip(addresses.iter_mut())
+                    .filter(|(_, a)| a.is_none())
+                {
                     if let Some(offset) = memmem::find(search_region, pattern) {
                         let found_address = unsafe { search_base.add(offset) } as *const c_void;
                         log::debug!("Found address {:#08X}", found_address as usize);
@@ -99,9 +115,17 @@ impl ByteSearcher {
         let mut bytes_needed = 0;
         let hproc = unsafe { GetCurrentProcess() };
         log::debug!("Enumerating process modules");
-        unsafe { EnumProcessModules(hproc, modules.as_mut_ptr(), size_of_val(&modules) as u32, &mut bytes_needed) }?;
+        unsafe {
+            EnumProcessModules(
+                hproc,
+                modules.as_mut_ptr(),
+                size_of_val(&modules) as u32,
+                &mut bytes_needed,
+            )
+        }?;
 
-        let num_modules = std::cmp::min(bytes_needed as usize / size_of::<HMODULE>(), modules.len());
+        let num_modules =
+            std::cmp::min(bytes_needed as usize / size_of::<HMODULE>(), modules.len());
         log::debug!("Found {} modules", num_modules);
         for &module in &modules[..num_modules] {
             let mut name_utf16 = [0; MAX_PATH as usize];
@@ -115,30 +139,53 @@ impl ByteSearcher {
                     Ok(name) => name,
                     Err(_) => continue,
                 }
-            }.to_lowercase();
+            }
+            .to_lowercase();
 
             log::debug!("Module name: {}", module_name);
             let mut module_info = MODULEINFO::default();
             unsafe {
-                GetModuleInformation(hproc, module, &mut module_info, size_of_val(&module_info) as u32)?;
+                GetModuleInformation(
+                    hproc,
+                    module,
+                    &mut module_info,
+                    size_of_val(&module_info) as u32,
+                )?;
                 let base = module_info.lpBaseOfDll as *const c_void;
-                self.modules.insert(module_name, (base, base.add(module_info.SizeOfImage as usize)));
+                self.modules.insert(
+                    module_name,
+                    (base, base.add(module_info.SizeOfImage as usize)),
+                );
             }
         }
 
         Ok(())
     }
 
-    fn get_module_ranges<'b, 'a: 'b, 'c: 'b>(&'a self, modules: &'b [&'c str]) -> impl Iterator<Item = &'a (*const c_void, *const c_void)> + 'b {
-        modules.iter().filter_map(|&module_name| self.modules.get(&module_name.to_lowercase()))
+    fn get_module_ranges<'b, 'a: 'b, 'c: 'b>(
+        &'a self,
+        modules: &'b [&'c str],
+    ) -> impl Iterator<Item = &'a (*const c_void, *const c_void)> + 'b {
+        modules
+            .iter()
+            .filter_map(|&module_name| self.modules.get(&module_name.to_lowercase()))
     }
 
-    pub fn find_bytes<const N: usize, const M: usize>(&self, patterns: &[&[u8]; N], protection: Option<PAGE_PROTECTION_FLAGS>, modules: &[&str; M]) -> Result<[Option<*const c_void>; N]> {
+    pub fn find_bytes<const N: usize, const M: usize>(
+        &self,
+        patterns: &[&[u8]; N],
+        protection: Option<PAGE_PROTECTION_FLAGS>,
+        modules: &[&str; M],
+    ) -> Result<[Option<*const c_void>; N]> {
         if M > 0 {
             Self::find_bytes_in_ranges(patterns, protection, self.get_module_ranges(modules))
         } else {
             // we'll use the standard page size as the minimum address
-            Self::find_bytes_in_ranges(patterns, protection, [&(0x1000 as *const c_void, usize::MAX as *const c_void)].into_iter())
+            Self::find_bytes_in_ranges(
+                patterns,
+                protection,
+                [&(0x1000 as *const c_void, usize::MAX as *const c_void)].into_iter(),
+            )
         }
     }
 }
